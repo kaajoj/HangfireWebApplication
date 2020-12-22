c2d8e7547718270fd4f833c85d787b7e2943aa3e
kaajoj <kaajooj@gmail.com>
2020-12-22 20:36:06 +0100

    Cleaning Startup.cs

--- HangfireWebApplicationTest/Startup.cs
+++ HangfireWebApplicationTest/Startup.cs
@@ -22,13 +22,11 @@ namespace HangfireWebApplicationTest
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -38,7 +36,6 @@ namespace HangfireWebApplicationTest
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();

9f273b3ab730876561b5323f2f486fadfd16da4c
kaajoj <kaajooj@gmail.com>
2020-12-22 20:48:13 +0100

    Add Hangfire services

--- HangfireWebApplicationTest/Startup.cs
+++ HangfireWebApplicationTest/Startup.cs
@@ -24,6 +24,26 @@ namespace HangfireWebApplicationTest
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add Hangfire services.
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+                {
+                    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                    QueuePollInterval = TimeSpan.Zero,
+                    UseRecommendedIsolationLevel = true,
+                    DisableGlobalLocks = true
+                }));
+
+            // Add the processing server as IHostedService
+            services.AddHangfireServer();
+
+            // Add framework services.
+            services.AddMvc();
+
             services.AddControllersWithViews();
         }
 
